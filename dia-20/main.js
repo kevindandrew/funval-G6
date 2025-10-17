5edc1a2e1a491843ab967c7b2867e61f22a813e9
kevindandrew <kevindandrew77@gmail.com>
2025-10-17 09:55:11 -0400

    Refactor code structure for improved readability and maintainability

---
+++ dia-20/main.js
@@ -0,0 +1,18 @@
+let btn = document.querySelector("#botoncito");
+let par = document.querySelector("#parrafo");
+btn.addEventListener("click", () => {
+  /*  if (btn.classList.contains("bg-green-800")) {
+    btn.classList.replace("bg-green-800", "bg-red-800");
+    btn.textContent = "Kevin estuvo aqui";
+  } else {
+    btn.classList.replace("bg-red-800", "bg-green-800");
+    btn.textContent = "haz click";
+  } */
+  par.textContent =
+    "funval tiene un programa de desarrollo web q consiste en 2 niveles";
+});
+/* function color() {
+  btn.classList.replace("bg-green-800", "bg-yellow-600");
+  btn.textContent = "boren estuvo aqui";
+}
+ */
