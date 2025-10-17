3e157e6e9c3dc1dc3d954cd4e3690faea9449123
kevindandrew <kevindandrew77@gmail.com>
2025-10-17 11:05:32 -0400

    agregando formulario para capturar nombre, edad y país; actualizando el HTML y JavaScript para manejar la entrada del usuario

--- dia-20/main.js
+++ dia-20/main.js
@@ -1,13 +1,13 @@
 let btn = document.querySelector("#botoncito");
 let par = document.querySelector("#parrafo");
 btn.addEventListener("click", () => {
-  /*  if (btn.classList.contains("bg-green-800")) {
+  if (btn.classList.contains("bg-green-800")) {
     btn.classList.replace("bg-green-800", "bg-red-800");
     btn.textContent = "Kevin estuvo aqui";
   } else {
     btn.classList.replace("bg-red-800", "bg-green-800");
     btn.textContent = "haz click";
-  } */
+  }
   par.textContent =
     "funval tiene un programa de desarrollo web q consiste en 2 niveles";
 });
@@ -16,3 +16,39 @@ btn.addEventListener("click", () => {
   btn.textContent = "boren estuvo aqui";
 }
  */
+
+/* 
+    crear un boton y una imagen
+    el boton debera alternar entre dia/noche
+    y la imagen en dia sera de la princesa fiona
+    y en noche de la ogro fiona
+*/
+
+let input = document.querySelector("#nombre");
+let par2 = document.querySelector("#par2");
+input.addEventListener("input", () => {
+  console.log(input.value);
+  par2.textContent = input.value;
+});
+
+/* ------------------------------- */
+let form = document.querySelector("#formulario");
+
+form.addEventListener("submit", (e) => {
+  //console.log("hola mundo");
+  e.preventDefault();
+  /* console.log("hola mundo");
+  console.log(e);
+  let nombreEstudiante = form.nombre.value;
+  let edad = form.edad.value;
+  let pais = form.pais.value;
+  console.log(nombreEstudiante, edad, pais);
+  let mensaje = `bienvenido ${nombreEstudiante} del pais de: ${pais} de edad:${edad}`;
+  console.log(mensaje); */
+  let datos = new FormData(form);
+  let nombreEstudiante = datos.get("nombre");
+  let edad = datos.get("edad");
+  let pais = datos.get("pais");
+  let mensaje = `bienvenido ${nombreEstudiante} del pais de: ${pais} de edad:${edad}`;
+  console.log(mensaje);
+});
